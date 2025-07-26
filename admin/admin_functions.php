694c84cfc8985e5a8cea139953e9a17398327f4a
Hamed <jamshidpour@hotmail.com>
2025-07-27 02:10:31 +0330

    Update admin_functions.php

--- admin/admin_functions.php
+++ admin/admin_functions.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) exit;
 
 // تابع نمایش صفحه مدیریت آزمون‌ها
 function wgm_render_test_manager_page() {
-    include WGM_PLUGIN_DIR . 'admin/test-manager.php';
+    include WGM_PLUGIN_DIR . 'admin/test_manager.php';
 }
 
 // سایر توابع مدیریتی می‌توانند اینجا اضافه شوند
